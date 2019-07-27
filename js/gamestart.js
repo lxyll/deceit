390f72ff38cb6b64057171b9e524715151545b3d
lxy <1689663541@qq.com>
2019-07-27 08:37:54 +0800

    新增页面

---
+++ js/gamestart.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $(".tab li").click(function () {
+        $(".tab li").eq($(this).index()).addClass("cur").siblings().removeClass("cur");
+        $(".status").hide().eq($(this).index()+1).show();
+    });
+});
+
+
+$(document).ready(function () {
+    $(".next span").click(function () {
+        if (!$(".tab li").is(".cur")) {
+            $(".hint").animate({
+                left: '5.2rem'
+            });
+            setTimeout(function () {
+                $(".hint").animate({
+                    left: '25rem'
+                });
+                setTimeout(function () {
+                    $(".hint").css({
+                        left: '-15rem'
+                    });
+                }, 1000);
+            }, 2000);
+            return false;
+        }
+        location.href = "password.html";
+    });
+});
